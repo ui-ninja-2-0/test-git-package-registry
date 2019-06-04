ed91667977107005281ed0be51560321877f047d
chirag borkar <cborkar@lordabbett.com>
2019-06-04 14:27:19 -0400

    removing unwanted files

--- stories/fundBasics.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {storiesOf} from '@storybook/react';
-
-import FundBasics from '../src/components/FundBasics';
-
-storiesOf('Fund Details', module).add('with test data', () => (
-    <FundBasics/>
-));
--- styleguide.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    title: 'Lord Abbett v3 components',
-    components: 'src/components/**/index.js',
-    styleguideDir: 'docs'
-};
